64e203f24b8e151da77ade3f8d0268fa2c101cba
Mia Dinh <miadinh38@gmail.com>
2024-08-15 21:38:31 -0400

    Minor typo

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -33,11 +33,11 @@ module.exports = {
         'bg-img-1': "url('/img-1.png')",
         'bg-img-2': "url('/img-2.png')",
         'feature-bg': "url('/feature-bg.png')",
-        pattern: "url('/pattern.png')",
+        'pattern': "url('/pattern.png')",
         'pattern-2': "url('/pattern-bg.png')",
       },
       screens: {
-        xs: '400px',
+        'xs': '400px',
         '3xl': '1680px',
         '4xl': '2200px',
       },
