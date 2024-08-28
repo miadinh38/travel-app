a682dc6c70809344690433f0f4ac7bb3227d56b5
Mia Dinh <miadinh38@gmail.com>
2024-08-27 20:41:10 -0400

    Update app name

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
-  title: "Camp Traveler",
+  title: "Hilink",
   description: "Travel UI/UX App for Camping",
 };
 
